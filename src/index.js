589a77846b6e5ba09b9c23dad1a5ebef3de37afe
Danko Lučić <dlucic33@gmail.com>
2017-12-16 19:59:27 +0100

    setup initial game layout

--- src/index.js
+++ src/index.js
@@ -2,6 +2,20 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import './index.css';
 
-const app = document.createElement('app');
-app.innerHTML = 'Hello React';
+function Title(props) {
+  return <h1>Hello {props.name}</h1>;
+}
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <Title name="React" />
+      </div>
+    );
+  }
+}
+
+const app = document.createElement('div');
 document.body.appendChild(app);
+ReactDom.render(<Game />, app);
