e1e8a7f678742fd50a732ec90381690ddfba5ce5
Danko Lučić <dlucic33@gmail.com>
2017-12-17 00:07:41 +0100

    cleaning up

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function Title({ name }) {
+export default function Title({ name }) {
   return <h1>Hello {name}</h1>;
 }
 
 Title.propTypes = {
   name: PropTypes.string.isRequired,
 };
-
-export default Title;
