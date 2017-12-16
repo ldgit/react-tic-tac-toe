0de57647d901a8029e01b1ddada2d6a0e698af55
Danko Lučić <dlucic33@gmail.com>
2017-12-16 18:46:24 +0100

    * added index file * setup babel * setup webpack build (js & css) * WIP webpack dev server

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    main: './src/index.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'web/build'),
+    filename: '[name].js',
+  },
+  devServer: {
+    contentBase: './web'
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ]
+      }
+    ]
+  },
+};
