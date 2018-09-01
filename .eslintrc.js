23e22bf1473a5bf92eaa87779edfc17662a63bce
Danko Lučić <dlucic33@gmail.com>
2018-09-02 01:05:01 +0200

    eslint max-len 120 characters warning

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,10 @@ module.exports = {
       "no-use-before-define": ["error", { "functions": false, "classes": true }],
       "react/prop-types": ["off"] ,
       "react/jsx-one-expression-per-line": ["off"],
+      'max-len': ['warn', 120, 2, {
+        'ignoreUrls': false,
+        'ignoreComments': false,
+      }],
     },
     "env": {
         "browser": true,
