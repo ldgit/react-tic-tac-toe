8f984cfa18a3b0636122e9bc36f793e62e5e18f7
Danko Lučić <dlucic33@gmail.com>
2018-11-30 19:57:41 +0100

    refactored LoadGame component to use hooks API

--- src/components/LoadGame.jsx
+++ src/components/LoadGame.jsx
@@ -1,23 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export default class LoadGame extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      gameToImport: {},
-    };
-    this.handleOnChange = this.handleOnChange.bind(this);
-    this.handleLoadGameClick = this.handleLoadGameClick.bind(this);
+export default function LoadGame({ onLoadGameClick }) {
+  const [gameToImport, setGameToImport] = useState({});
+
+  function handleOnChange(event) {
+    setGameToImport(event.target.value);
   }
 
-  handleOnChange(event) {
-    this.setState({ gameToImport: event.target.value });
-  }
-
-  handleLoadGameClick() {
-    const { gameToImport } = this.state;
-    const { onLoadGameClick } = this.props;
-
+  function handleLoadGameClick() {
     if (!isValidJSON(gameToImport)) {
       // eslint-disable-next-line no-alert
       window.alert('Invalid save game JSON');
@@ -27,18 +17,13 @@ export default class LoadGame extends React.Component {
     onLoadGameClick(JSON.parse(gameToImport));
   }
 
-  render() {
-    return (
-      <span>
-        <textarea
-          data-testid="importGameTextarea"
-          onChange={this.handleOnChange}
-        />
-        <br />
-        <button type="button" onClick={this.handleLoadGameClick}>Load game</button>
-      </span>
-    );
-  }
+  return (
+    <span>
+      <textarea data-testid="importGameTextarea" onChange={handleOnChange} />
+      <br />
+      <button type="button" onClick={handleLoadGameClick}>Load game</button>
+    </span>
+  );
 }
 
 function isValidJSON(jsonString) {
