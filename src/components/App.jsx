adcd0d55a0140c5c58f02dad5dcb637d92ecef80
Danko Lucic <dlucic33@gmail.com>
2019-06-06 18:54:31 +0200

    * removed classic game * stylesheet cleanup

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,13 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import Game from './Game';
 import Title from './Title';
 import UltimateGame from './UltimateGame';
 
 function App() {
   return (
-    <div className="table">
-      <div className="table-cell table-small-padding">
-        <Title name="React" />
-        <UltimateGame />
-      </div>
-      <div className="table-cell table-large-padding">
-        <h2>The classic!</h2>
-        <Game />
-      </div>
+    <div className="table-cell table-small-padding">
+      <Title name="React" />
+      <UltimateGame />
     </div>
   );
 }
