f9c4015e5e541426175211c39812c913c4146dec
Danko Lučić <ld.is.a.git@gmail.com>
2021-01-23 01:44:57 +0100

    updated all npm packages to latest version

--- webpack.config.js
+++ webpack.config.js
@@ -2,57 +2,55 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-module.exports = (env, argv) => ({
-  entry: {
-    main: './src/index.js',
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].[chunkhash].js',
-  },
-  optimization: {
-    splitChunks: { chunks: 'all' },
-  },
-  devtool: argv.mode === 'development' ? 'inline-source-map' : 'none',
-  devServer: {
-    contentBase: './dist',
-  },
-  resolve: {
-    extensions: ['.js', '.json', '.jsx'],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(jsx|js)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
+module.exports = (env, argv) => {
+  const config = {
+    entry: {
+      main: './src/index.js',
+    },
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: '[name].[chunkhash].js',
+    },
+    optimization: {
+      splitChunks: { chunks: 'all' },
+    },
+    devServer: {
+      contentBase: './dist',
+    },
+    resolve: {
+      extensions: ['.js', '.json', '.jsx'],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(jsx|js)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+          },
         },
-      },
-      {
-        test: /\.sass$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
-      },
-      {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: ['file-loader'],
-      },
+        {
+          test: /\.sass$/,
+          use: ['style-loader', 'css-loader', 'sass-loader'],
+        },
+        {
+          test: /\.(png|svg|jpg|gif)$/,
+          use: ['file-loader'],
+        },
+      ],
+    },
+    plugins: [
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        template: 'src/index.html',
+        minify: true,
+      }),
     ],
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      template: 'src/index.html',
-      inject: false,
-      // Until v4, see https://github.com/jantimon/html-webpack-plugin/issues/1094#issuecomment-434567656
-      minify: {
-        collapseWhitespace: true,
-        removeComments: true,
-        removeRedundantAttributes: true,
-        removeScriptTypeAttributes: true,
-        removeStyleLinkTypeAttributes: true,
-        useShortDoctype: true,
-      },
-    }),
-  ],
-});
+  };
+
+  if (argv.mode === 'development') {
+    config.devtool = 'eval-source-map';
+  }
+
+  return config;
+};
